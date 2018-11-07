c90dbb78d47a5217d646838590625c1fe6233711
Duy Truong <truongkhanhduy9@gmail.com>
2018-11-07 16:06:08 +0700

    Format file

--- nhaccuatui/nhaccuatui/spiders/lyrics_spider.py
+++ nhaccuatui/nhaccuatui/spiders/lyrics_spider.py
@@ -8,15 +8,15 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        finalPage = response.xpath('//div[@class="box-content"]/div[@class="wrap"]/div[@class="content-wrap"]/div[@class="box-left"]/div[@class="box_pageview"]/a/@href')[-1].extract()
-        totalPage = int(finalPage.split(".")[-2])
-        for page in range(totalPage):
-            link = finalPage.replace(str(totalPage), str(page + 1))
-            yield scrapy.Request(link, callback=self.crawlLyric)
+      finalPage = response.xpath('//div[@class="box-content"]/div[@class="wrap"]/div[@class="content-wrap"]/div[@class="box-left"]/div[@class="box_pageview"]/a/@href')[-1].extract()
+      totalPage = int(finalPage.split(".")[-2])
+      for page in range(totalPage):
+          link = finalPage.replace(str(totalPage), str(page + 1))
+          yield scrapy.Request(link, callback=self.crawlLyric)
 
     def crawlLyric(self, response):
-        for linkLyric in response.xpath('//div[@class="box-content"]/div[@class="wrap"]/div[@class="content-wrap"]/div[@class="box-left"]/div[@class="list_music_full"]/div[@class="fram_select"]/ul[@class="list_item_music"]/li/a[@class="button_new_window"]/@href').extract():
-            yield scrapy.Request(linkLyric, callback=self.saveFile)
+      for linkLyric in response.xpath('//div[@class="box-content"]/div[@class="wrap"]/div[@class="content-wrap"]/div[@class="box-left"]/div[@class="list_music_full"]/div[@class="fram_select"]/ul[@class="list_item_music"]/li/a[@class="button_new_window"]/@href').extract():
+          yield scrapy.Request(linkLyric, callback=self.saveFile)
 
     def saveFile(self, response):
       lyricRaw = response.xpath('//div[@class="box-content"]/div[@class="wrap"]/div[@class="content-wrap"]/div[@class="box-left"]/div[@class="lyric"]/p[@id="divLyric"]/text()').extract()
