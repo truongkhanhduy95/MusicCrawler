c8a17089c65c479950aa7af9d7f1eacf9a6e9620
Duy Truong <truongkhanhduy9@gmail.com>
2018-11-12 16:41:03 +0700

    Update splash load script

--- nhaccuatui/nhaccuatui/settings.py
+++ nhaccuatui/nhaccuatui/settings.py
@@ -26,6 +26,22 @@ MYSQL_USER = "root"
 MYSQL_PASSWORD = "admin"
 MYSQL_PORT = "3306"
 
+# Splash 
+SPLASH_URL = 'http://localhost:8050'
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
+COOKIES_ENABLED = True # Nếu cần dùng Cookie
+SPLASH_COOKIES_DEBUG = False
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': 400,
+}
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'nhaccuatui (+http://www.yourdomain.com)'
 
